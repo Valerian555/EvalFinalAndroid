e3a07e8f7ad1a922b3ab4967907ea5aaaf6162de
Valérian P <129851526+Valerian555@users.noreply.github.com>
2023-10-05 12:08:47 +0200

    fix bottombar bug

--- app/src/main/java/com/technipixl/evalfinaleandroid/MainActivity.kt
+++ app/src/main/java/com/technipixl/evalfinaleandroid/MainActivity.kt
@@ -3,6 +3,8 @@ package com.technipixl.evalfinaleandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.technipixl.evalfinaleandroid.databinding.ActivityMainBinding
 import com.technipixl.evalfinaleandroid.ui.search.SearchFragment
 import com.technipixl.evalfinaleandroid.ui.trending.TrendingFragment
@@ -13,26 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        }
 
-        //gestion de la bottombar
-        binding.myBottombar.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.searchFragment -> {
-                    loadFragment(SearchFragment())
-                    true
-                }
+        override fun onStart() {
+            super.onStart()
 
-                else -> {
-                    loadFragment(TrendingFragment())
-                    true
-                }
-            }
+            //gestion de ma bottombar
+            val bottomNavigationView = binding.myBottombar
+            val navController = findNavController(R.id.nav_host_fragment)
+            NavigationUI.setupWithNavController(bottomNavigationView, navController)
         }
     }
-
-    private  fun loadFragment(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.nav_host_fragment,fragment)
-        transaction.commit()
-    }
-}
